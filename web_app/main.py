09962f3363aa0f1a70f3f943b7dd11dcbbb6c592
krz144 <krzysztof.kc11@gmail.com>
2022-05-16 22:13:05 +0200

    zadanie 1.1 i 1.2

--- web_app/main.py
+++ web_app/main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def root():
-    return {"message": "zmiana Hello World"}
+    return {"start": "1970-01-01"}
+
+
+@app.api_route("/test", methods=['GET', 'POST'])
+def testoowanko(request):
+    return f'Client used {request.method} method'
+
+
+@app.get('/method', status_code=status.HTTP_200_OK)
+def methodget():
+    return {"method": "GET"}
+
+
+@app.put('/method', status_code=status.HTTP_200_OK)
+def methodput():
+    return {"method": "PUT"}
+
+
+@app.delete('/method', status_code=status.HTTP_200_OK)
+def methoddelete():
+    return {"method": "DELETE"}
+
+
+@app.options('/method', status_code=status.HTTP_200_OK)
+def methodoptions():
+    return {"method": "OPTIONS"}
+
+
+@app.post('/method', status_code=status.HTTP_201_CREATED)
+def methodpost():
+    return {"method": "POST"}
